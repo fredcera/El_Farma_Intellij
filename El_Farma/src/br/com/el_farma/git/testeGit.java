131392ed40a7cd1fe4943370812e68dd9c27e0d0
Frederico Ackermann Cera <frodacera@hotmail.com>
2021-04-28 17:32:21 -0300

    Teste de atualização e integração com Intellij

--- El_Farma/src/br/com/el_farma/git/testeGit.java
+++ El_Farma/src/br/com/el_farma/git/testeGit.java
@@ -3,11 +3,11 @@ package br.com.el_farma.git;
 /**
  * @author froda
  * Exemplo teste de integração com o Git
- *
  */
 public class testeGit {
 
     public static void main(String[] args) {
         System.out.println("Testando Git!");
+        System.out.println("Criado o teste por Frederico!");
     }
 }
