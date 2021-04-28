160233efbd28ce0b67a3ff04d960164494c825eb
Frederico Ackermann Cera <73366611+fredcera@users.noreply.github.com>
2021-04-28 17:07:13 -0300

    Adicionei os arquivos inicias para integração de Intellij

---
+++ El_Farma/src/br/com/el_farma/git/testeGit.java
@@ -0,0 +1,13 @@
+package br.com.el_farma.git;
+
+/**
+ * @author froda
+ * Exemplo teste de integração com o Git
+ *
+ */
+public class testeGit {
+
+    public static void main(String[] args) {
+        System.out.println("Testando Git!");
+    }
+}
